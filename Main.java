de2e66dd8dbc3209e16e21a11bc23510a10c3e65
khalilbiz28 <129905670+khalilbiz28@users.noreply.github.com>
2025-06-11 10:38:30 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,19 @@
 class Main {
   public static void main(String[] args) {
-    int a = Integer.parseInt(args[0]);
-    int b = Integer.parseInt(args[1]);
-    String op = args[2];
-
-    switch(op) {
-      case "+":
-        System.out.println(a + b);
-        break;
-      case "-":
-        System.out.println(a - b);
-        break;
-      case "":
-        System.out.println(a b);
-        break;
-      case "/":
-        if (b != 0) {
-          System.out.println(a / b);
-        } else {
-          System.out.println("Division par zéro interdite");
-          System.exit(-1);
-        }
-        break;
-      default:
-        System.out.println("Opérateur non reconnu");
-        System.exit(-1);
+    if(args[(args.length-1)].equals("+")){
+      System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+    }
+    else if(args[(args.length-1)].equals("-")){
+      System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[1]));
+    }
+    else if(args[(args.length-1)].equals("*")){
+      System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
+    }
+    else if(args[(args.length-1)].equals("/")){
+      System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[1]));
+    }
+    else {
+      System.exit(-1);
     }
   }
 }
